890bf4e36b31e4b64af2908cefc21b3e8ca4a501
Davide Tello <tello.davide@gmail.com>
2021-05-16 09:01:12 +0200

    Add City List action and reducer

--- src/containers/city.jsx
+++ src/containers/city.jsx
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class City extends Component {
-    render() {
-        return <div className="list-group-item">{this.props.city.name}</div>;
-    }
+  render() {
+    return <div className="list-group-item">{this.props.city.name}</div>;
+  }
 }
 
 export default City;
